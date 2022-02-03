c488a8d556bc38da458048413d9a4ff4ccf6376e
saidi <saidi.ahamada@gmail.com>
2022-02-03 09:32:51 +0100

    ci: mock notifier service cause no hub in ci

--- tests/Command/SendNotificationCommandTest.php
+++ tests/Command/SendNotificationCommandTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\App\Tests\Command;
 
+use App\Service\Notification\Notifier;
+use App\Service\Notification\NotifierInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -11,6 +13,17 @@ class SendNotificationCommandTest extends KernelTestCase
     public function testExecute(): void
     {
         self::bootKernel();
+
+        $notifierMock = $this->getMockBuilder(NotifierInterface::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['send'])
+            ->getMock();
+
+        $notifierMock->expects($this->once())
+            ->method('send')
+            ->willReturn('urn:uuid:7b26df22-bbc5-466f-b517-05aaa3b12f4a');
+
+        self::$kernel->getContainer()->set(Notifier::class, $notifierMock);
 
         $command = (new Application(self::$kernel))->find('app:send-notif');
 
